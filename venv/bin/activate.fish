b12440e7701c0f6d152435d3dde5a7f6bb263f37
Prashant <prashantupreti9844@gmail.com>
2026-03-23 02:54:13 +0530

    notebook

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/prashant/Desktop/MLprojec/venv
+set -gx VIRTUAL_ENV '/home/prashant/Desktop/ML Project/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
